fafdf27936fc2582ee114137fe99460b4528549f
Segun Ade <giftsegunade@gmail.com>
2025-05-26 16:03:48 +0100

    configure initial routing

--- core/Router.php
+++ core/Router.php
@@ -1,14 +1,29 @@
-<?php 
+<?php
+
 namespace app\core;
-class Router 
+
+class Router
 {
+  public Request $request;
   protected array $routes = [];
-  public function get(string $path, callable $callback) 
+  public function __construct(Request $request)
+  {
+    $this->request = $request;
+  }
+  public function get(string $path, callable $callback)
   {
     $this->routes['get'][$path] = $callback;
   }
-  public function resolve() 
+  public function resolve()
   {
-
+    $path =  $this->request->getPath();
+    $method = $this->request->getMethod();
+    $callback = $this->routes[$method][$path] ?? false;
+    if ($callback === false) {
+      echo "Not Found";
+      exit;
+    }
+    echo call_user_func($callback);
+    
   }
 }
