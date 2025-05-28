32c4654fb6881d57153ae50f6b36733d5b41e70e
Segun Ade <giftsegunade@gmail.com>
2025-05-28 12:49:39 +0100

    display contact form

--- core/Router.php
+++ core/Router.php
@@ -16,6 +16,10 @@ class Router
   {
     $this->routes['get'][$path] = $callback;
   }
+  public function post(string $path, $callback)
+  {
+    $this->routes['post'][$path] = $callback;
+  }
   public function resolve()
   {
     $path =  $this->request->getPath();
